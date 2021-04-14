d6c2391c370ef27d4fce7d523dd5bdc7c1f58584
lgramu97 <gramuglialautaro@gmail.com>
2021-04-14 16:46:19 -0300

    Llamadas a servidores concurrente

---
+++ src/go_servidores.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	inicio := time.Now()
+
+	canal := make(chan string) // establezco nuevo canal para enviar datos y leer.
+
+	servidores := []string{
+		"http://platzi.com",
+		"http://google.com",
+		"http://facebook.com",
+		"http://instagram.com",
+	}
+
+	for _, valor := range servidores {
+		go revisarServidor(valor, canal)
+	}
+
+	fin := time.Since(inicio)
+	fmt.Printf("Finalizado request %s \n", fin)
+
+	for i := 0; i < len(servidores); i++ {
+		fmt.Println(<-canal)
+	}
+
+	fin = time.Since(inicio)
+	fmt.Printf("Finalizado response %s \n", fin)
+
+}
+
+func revisarServidor(servidor string, canal chan string) {
+	_, error := http.Get(servidor)
+	if error != nil {
+		fmt.Println(servidor, "El servidor esta caido.")
+		canal <- servidor + " No se encuentra disponible"
+	} else {
+		fmt.Println(servidor, "El servidor esta funcionando correctamente.")
+		canal <- servidor + " Servidor funcionando."
+	}
+}

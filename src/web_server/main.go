d37ea57776a1bc7f5b2d41794f5113d1dc8ea1d8
lgramu97 <gramuglialautaro@gmail.com>
2021-04-16 12:54:33 -0300

    Manejo de N middlewares

--- src/web_server/main.go
+++ src/web_server/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	server := NewServer(":3000")
 	server.Handle("/", HandleRoot)
-	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth()))
+	server.Handle("/api", server.AddMiddleware(HandleHome, CheckAuth(), Login()))
 
 	server.Listen()
 }
--- src/web_server/middleware.go
+++ src/web_server/middleware.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 // MiddleWare para chequear si esta o no autenticado.
 func CheckAuth() MiddleWare {
 	return func(f http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
+			// Logica del middleware
 			flag := true
 			fmt.Println("Checking authentication")
 			if flag { // si pasa el middleware llama al siguiente handler(f).
@@ -16,6 +19,19 @@ func CheckAuth() MiddleWare {
 			} else {
 				return
 			}
+		} // retorno un handler.
+	}
+}
+
+func Login() MiddleWare {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			// Logica del middleware
+			start := time.Now()
+			defer func() { // Se ejecuta al final-
+				log.Println(r.URL.Path, time.Since(start))
+			}()
+			f(w, r)
 		} // retorno un handler.
 	}
 }
